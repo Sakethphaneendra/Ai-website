15c800f4d548613eea39b738327e2875e2b413d8
K.Phaneendra Saketh <71480116+Sakethphaneendra@users.noreply.github.com>
2023-04-17 01:35:39 +0530

    Late update on 17-04-2023 for ai website

---
+++ show_hide.js
@@ -0,0 +1,12 @@
+var a;
+function show_hide(){
+    if(a==1){
+            document.getElementById("hide").style.visibility="none";
+            return a=0;   
+    }
+    else{
+        document.getElementById("hide").style.visibility="none";
+            return a=1; 
+    }
+}
+
